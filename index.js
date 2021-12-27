963251b1ee15c6b8c86e24610bc5314514f9ebfa
Tim Holman <timothy.w.holman@gmail.com>
2021-12-27 16:25:15 -0500

    1 square minesweeper

---
+++ index.js
@@ -0,0 +1,100 @@
+(() => {
+  const digit = document.querySelector(".digit");
+
+  const timer0 = document.querySelector(".timer-0");
+  const timer1 = document.querySelector(".timer-1");
+  const timer2 = document.querySelector(".timer-2");
+
+  const faceButton = document.querySelector(".face-button");
+
+  const oneSquare = document.querySelector(".one-square");
+
+  let clicking = false;
+  let dead = false;
+
+  let startTime, aniamtionFrame
+
+  startTimer()
+
+  document.oncontextmenu = function (e) {
+    if(e.target === oneSquare) return false
+  };
+
+  function reset() {
+    dead = false;
+    win = false;
+    faceButton.classList.remove("face-button-clicking");
+    faceButton.classList.remove("face-button-dead");
+    faceButton.classList.remove("face-button-won");
+    oneSquare.classList.remove("exploded");
+    oneSquare.classList.remove("flagged");
+    digit.src = "./img/display-digit-1.svg";
+    startTimer()
+  }
+
+  function startTimer() {
+    startTime = Date.now();
+    animationFrame = window.requestAnimationFrame(tick);
+  }
+
+  function tick() {
+    setTime(Date.now() - startTime)
+
+    animationFrame = window.requestAnimationFrame(tick);
+  }
+
+  function setTime(duration) {
+    let seconds = Math.floor((duration / 1000) % 60)
+    
+    if(seconds > 999) {
+      seconds = 999;
+    }
+
+    const secondString = seconds.toString().padStart(3, 0).split('')
+    timer0.src = `./img/display-digit-${secondString[0]}.svg`;
+    timer1.src = `./img/display-digit-${secondString[1]}.svg`;
+    timer2.src = `./img/display-digit-${secondString[2]}.svg`;
+  }
+
+  oneSquare.addEventListener("mousedown", (e) => {
+    if (e.ctrlKey) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+    clicking = true;
+    faceButton.classList.add("face-button-clicking");
+  });
+
+  oneSquare.addEventListener("mouseup", (e) => {
+    if (clicking === false) {
+      return;
+    }
+
+    if(e.ctrlKey || e.which === 3) {
+      faceButton.classList.remove("face-button-clicking");
+      oneSquare.classList.add("flagged");
+      faceButton.classList.add("face-button-won");
+      digit.src = "./img/display-digit-0.svg";
+    } else {
+      faceButton.classList.remove("face-button-clicking");
+      oneSquare.classList.add("exploded");
+      faceButton.classList.add("face-button-dead");
+      digit.src = "./img/display-digit-1.svg";
+    }
+
+    cancelAnimationFrame(animationFrame)
+    clicking = false;
+    
+  });
+
+  oneSquare.addEventListener("mouseout", () => {
+    if (clicking === true) {
+      clicking = false;
+      faceButton.classList.remove("face-button-clicking");
+    }
+  });
+
+  faceButton.addEventListener("click", () => {
+    reset();
+  });
+})();
