b1b7ee83321f86f0b83dcbdda565ec91f3baf2cd
Tim Holman <timothy.w.holman@gmail.com>
2021-12-27 20:08:45 -0500

    small changes

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
+
+
+
 (() => {
   const digit = document.querySelector(".digit");
-
   const timer0 = document.querySelector(".timer-0");
   const timer1 = document.querySelector(".timer-1");
   const timer2 = document.querySelector(".timer-2");
-
   const faceButton = document.querySelector(".face-button");
-
   const oneSquare = document.querySelector(".one-square");
 
-  let clicking = false;
-  let dead = false;
+  let startTime, animationFrame;
+  let last = ''
 
-  let startTime, aniamtionFrame
-
-  startTimer()
+  startTimer();
 
   document.oncontextmenu = function (e) {
-    if(e.target === oneSquare) return false
+    if (e.target === oneSquare) return false;
   };
 
   function reset() {
+    cancelAnimationFrame(animationFrame);
     dead = false;
     win = false;
     faceButton.classList.remove("face-button-clicking");
@@ -28,8 +27,8 @@
     faceButton.classList.remove("face-button-won");
     oneSquare.classList.remove("exploded");
     oneSquare.classList.remove("flagged");
-    digit.src = "./img/display-digit-1.svg";
-    startTimer()
+    digit.src = "./img/display-digit-1.png";
+    startTimer();
   }
 
   function startTimer() {
@@ -38,22 +37,21 @@
   }
 
   function tick() {
-    setTime(Date.now() - startTime)
-
+    setTime(Date.now() - startTime);
     animationFrame = window.requestAnimationFrame(tick);
   }
 
   function setTime(duration) {
-    let seconds = Math.floor((duration / 1000) % 60)
-    
-    if(seconds > 999) {
+    let seconds = Math.floor((duration / 1000) % 60);
+
+    if (seconds > 999) {
       seconds = 999;
     }
 
-    const secondString = seconds.toString().padStart(3, 0).split('')
-    timer0.src = `./img/display-digit-${secondString[0]}.svg`;
-    timer1.src = `./img/display-digit-${secondString[1]}.svg`;
-    timer2.src = `./img/display-digit-${secondString[2]}.svg`;
+    const secondString = seconds.toString().padStart(3, 0).split("");
+    timer0.src = `./img/display-digit-${secondString[0]}.png`;
+    timer1.src = `./img/display-digit-${secondString[1]}.png`;
+    timer2.src = `./img/display-digit-${secondString[2]}.png`;
   }
 
   oneSquare.addEventListener("mousedown", (e) => {
@@ -70,21 +68,20 @@
       return;
     }
 
-    if(e.ctrlKey || e.which === 3) {
+    if (e.ctrlKey || e.which === 3) {
       faceButton.classList.remove("face-button-clicking");
       oneSquare.classList.add("flagged");
       faceButton.classList.add("face-button-won");
-      digit.src = "./img/display-digit-0.svg";
+      digit.src = "./img/display-digit-0.png";
     } else {
       faceButton.classList.remove("face-button-clicking");
       oneSquare.classList.add("exploded");
       faceButton.classList.add("face-button-dead");
-      digit.src = "./img/display-digit-1.svg";
+      digit.src = "./img/display-digit-1.png";
     }
 
-    cancelAnimationFrame(animationFrame)
+    cancelAnimationFrame(animationFrame);
     clicking = false;
-    
   });
 
   oneSquare.addEventListener("mouseout", () => {
